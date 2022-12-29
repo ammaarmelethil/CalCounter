9d4128db69cf15936ca415f703e16e9adf2f2747
Ammaar Melethil <ammaarmelethil@outlook.com>
2022-12-29 14:55:38 -0700

    Initial Commit

---
+++ CalCounter/CalCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalCounterApp.swift
+//  CalCounter
+//
+//  Created by Ammaar Melethil on 2022-12-29.
+//
+
+import SwiftUI
+
+@main
+struct CalCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
